50c0544bfe353bb8881c02e317b07b9d2ae67940
Kaleshe <kalesheav@gmail.com>
2021-03-22 23:42:51 +0000

    'Updated'

--- functions/users.php
+++ functions/users.php
@@ -25,24 +25,26 @@ function add_user_to_school_cpt( $user_id )
     $user_info = get_userdata( $user_id );
 
     // Create a new post
-    $user_post = array(
+    $school_post = array(
         'post_title'   => get_field('name', 'user_' . $user_id), 
         'post_content' => $user_info->description,
         'post_type'    => 'school',
-        'post_status'   => 'publish',
+        'post_status'  => 'publish',
+        'post_author'  => $user_id,
     );
 
     // Insert the post into the database
-    $post_id = wp_insert_post( $user_post );
+    $school_id = wp_insert_post( $school_post );
+
+    // Add the school ID as a meta data
+    add_user_meta( $user_id, 'school_id', [$school_id] );
 
     // Add school custom fields
-    add_post_meta( $post_id, 'post_id', $post_id );
-    add_post_meta( $post_id, 'school_id', $user_info->ID );
-    add_post_meta( $post_id, 'email', $user_info->user_email );
-    add_post_meta( $post_id, 'school_telephone', get_field('telephone', 'user_' . $user_id) );
-    add_post_meta( $post_id, 'school_city', get_field('city', 'user_' . $user_id) );
-    add_post_meta( $post_id, 'school_postcode', get_field('postcode', 'user_' . $user_id) );
-    add_post_meta( $post_id, 'school_address', get_field('address', 'user_' . $user_id) );
+    add_post_meta( $school_id, 'email', $user_info->user_email );
+    add_post_meta( $school_id, 'school_telephone', get_field('telephone', 'user_' . $user_id) );
+    add_post_meta( $school_id, 'school_city', get_field('city', 'user_' . $user_id) );
+    add_post_meta( $school_id, 'school_postcode', get_field('postcode', 'user_' . $user_id) );
+    add_post_meta( $school_id, 'school_address', get_field('address', 'user_' . $user_id) );
 }
 
  /*
@@ -54,21 +56,22 @@ function update_school_cpt( $user_id, $old_user_data ) {
 
     // Get user info
     $user_info = get_userdata( $user_id );
-    $meta = get_post_meta( $user_id );
-    $post_id = $meta['post_id'][0];
 
-    $user_post = array(
-        'ID'           => $post_id,
+    // Get corresponding school ID
+    $school_id = get_user_meta( $user_id );
+    die(print_r($school_id, true));
+
+    $school_post = array(
+        'ID'           => $school_id,
         'post_title'   => get_field('name', 'user_' . $user_id), 
         'post_content' => $user_info->description,
     );
 
-    wp_update_post( $user_post );
+    wp_update_post( $school_post, true );
 
-    update_post_meta( $post_id, 'school_id', $user_info->ID );
-    update_post_meta( $post_id, 'email', $user_info->user_email );
-    update_post_meta( $post_id, 'school_telephone', get_field('telephone', 'user_' . $user_id) );
-    update_post_meta( $post_id, 'school_city', get_field('city', 'user_' . $user_id) );
-    update_post_meta( $post_id, 'school_postcode', get_field('postcode', 'user_' . $user_id) );
-    update_post_meta( $post_id, 'school_address', get_field('address', 'user_' . $user_id) );
+    update_post_meta( $school_id, 'email', $user_info->user_email );
+    update_post_meta( $school_id, 'school_telephone', get_field('telephone', 'user_' . $user_id) );
+    update_post_meta( $school_id, 'school_city', get_field('city', 'user_' . $user_id) );
+    update_post_meta( $school_id, 'school_postcode', get_field('postcode', 'user_' . $user_id) );
+    update_post_meta( $school_id, 'school_address', get_field('address', 'user_' . $user_id) );
 }
